88a271e3bae0205c82b7ee3e74c1aa8f3b935369
amar2610 <148216977+amar2610@users.noreply.github.com>
2024-06-30 13:07:17 +0530

    Add files via upload

---
+++ question2/src/question2/const1.java
@@ -0,0 +1,14 @@
+package question2;
+
+public class const1 {
+ String name;
+	public const1() {
+		this.name="Unknown";
+		System.out.println("student name is "+name);
+	}
+	public const1(String name) {
+		this.name=name;
+		System.out.println("student name is "+name);
+	}
+
+}
---
+++ question2/src/question2/defaultcon.java
@@ -0,0 +1,13 @@
+package question2;
+import java.util.Scanner;
+public class defaultcon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s1=new Scanner(System.in);
+		const1 c1=new const1();
+		const1 c2=new const1("amar");
+		
+	}
+
+}
