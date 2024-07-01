6b39ad3d15918246fa0e617632c346d254aba0af
amar2610 <148216977+amar2610@users.noreply.github.com>
2024-07-01 21:47:19 +0530

    Add files via upload

---
+++ finalvar/finalVar.java
@@ -0,0 +1,16 @@
+public class finalVar {
+
+    // final int sal=900;
+    final int sal;
+
+    // public finalVar{
+    // sal=9000;
+    // }
+    // We cannot do;
+    // public void shw(){
+    // sal++;
+    // }
+    public static void main(String[] args) {
+
+    }
+}
